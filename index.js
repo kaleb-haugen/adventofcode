83eb96d503adde14236e0956f657b8c187da5d07
kaleb-haugen <73085536+kaleb-haugen@users.noreply.github.com>
2022-12-02 18:31:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// Import stylesheets
-require('./2022-12-01/app.js');
+// Import stylesheet
+//import 
+
+// Import applications
+const day1 = require('./2022-12-01/app.js');
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `
-  <h1>JS Starter</h1>
-  <p>Results:</p>
-  `;
+const result1 = document.getElementById('d1');
+result1.className = `visible`;
+result1.innerHTML = `Day 1 Results:
+                  <ul>
+                    <li>69281</li>
+                    <li>201524</li>
+                  </ul>`;
