e6832891490dcbbba24800767fcedfdb21529d1c
Kaleb_Haugen <seldom_laser_0g@icloud.com>
2022-12-04 22:46:39 -0600

    Grr...still not working right LOL

--- 2022-12-03/app.js
+++ 2022-12-03/app.js
@@ -1,28 +1,81 @@
+// Split the string into an array
+splitByLine = (str, delim) =>
+  (str.split(delim));
+
+// Remove duplicate characters in the array values
+removeDupChar = (arr, index) =>
+  (Object.values(arr[index]).filter((v, w, x) => x.indexOf(v) === w).join(``));
+
+// Search nested array objects for shared characters
+findSharedChar = (arr, index, subindex) => {
+  for (n = 0; n < 128; n++) { // I was too lazy to check the actual length of the objects
+    let charReference = (arr[index][subindex + 1].charAt(n));
+    resultsArray.push(arr[index][subindex].search(charReference) >= 0 ? charReference : null);
+  }
+}
+
 // Import values as string
 let valuesString = document.getElementById(`inputs`).textContent.replace(/  /gm, ``);
 
+// // Part 1 /////////////////////////////////////////////////////////////////////////////////////////
+let valuesArray = [];
+let resultsArray = [];
+let position = 0;
+// // Split the string into an array
+// valuesArray = splitByLine(valuesString, `\n`);
+// // Split array objects into nested arrays and remove duplicate characters within array objects
+// for (i = 0; i < valuesArray.length; i++) {
+//   position = RegExp(`(.{1,` + (valuesArray[i].length / 2) + `})`, `g`);
+//   valuesArray[i] = valuesArray[i].split(position).filter(c => c !== ``);
+//   // Remove duplicate characters in the array values
+//   valuesArray[i][0] = removeDupChar(valuesArray, i, 0);
+//   valuesArray[i][1] = removeDupChar(valuesArray, i, 1);
+// };
+// // Search nested array objects for shared characters
+// for (a = 0; a < valuesArray.length; a++) {
+//   findSharedChar(valuesArray, a, 0)
+// };
+// // Remove null results
+// resultsArray = Object.values(resultsArray).filter(x => x);
+
+// Part 2 /////////////////////////////////////////////////////////////////////////////////////////
+let valuesArray1 = []
+let resultsArray1 = []
+let position1 = 0
 // Split the string into an array
-let valuesArray = valuesString.split(`\n`);
-
-// Split array objects into nested arrays
-for (i = 0; i < valuesArray.length; i++) {
-  let position = RegExp(`(.{1,` + (valuesArray[i].length / 2) + `})`, `g`);
-  valuesArray[i] = valuesArray[i].split(position).filter(c => c !== '');
-  // Remove duplicate characters in the array values
-  valuesArray[i][0] = Object.values(valuesArray[i][0]).filter((v, i, a) => a.indexOf(v) === i).join(``);
-  valuesArray[i][1] = Object.values(valuesArray[i][1]).filter((v, i, a) => a.indexOf(v) === i).join(``);
+valuesArray1 = splitByLine(valuesString, `\n`);
+// Remove duplicate characters
+for (i = 0; i < valuesArray1.length; i++) {
+  console.log(eval(valuesArray1 + `[` + i + `]`));
+  valuesArray1[i] = removeDupChar(valuesArray1,i);// `[` + i + `]`);
+  console.log(eval(valuesArray1 + `[` + i + `]`));
 };
+// console.log(valuesArray1);
+// let resultsArray1 = []
+// valuesArray1.forEach(i => {
+//   // Remove duplicate characters in the array values
+//   valuesArray[i] = Object.values(valuesArray1[i]).filter((v, i, a) => a.indexOf(v) === i).join(``);
+// });
+// console.log(valuesArray1)
 
-// Search nested array objects for shared characters
-let resultsArray = []
-for (a = 0; a < valuesArray.length; a++) {
-  for (s = 0; s < valuesArray[a][1].length; s++) {
-    let charReference = (valuesArray[a][1].charAt(s));
-    resultsArray.push(valuesArray[a][0].search(charReference) >= 0 ? charReference : null);
-  }
-}
-// Remove null results
-resultsArray = Object.values(resultsArray).filter(x => x);
+// // Split the string into an array of objects each containing 3 lines
+// function modify(str, n, delim) {
+//   return str.split(delim).reduce(function (output, item, i) {
+//     if (!(i % n)) {
+//       output.push(item);
+//     } else {
+//       output[i / n | 0] += delim + item;
+//     };
+//     return output;
+//   }, []);
+// };
+// // let valuesArray1 = modify(valuesString, 3, `\n`)
+// // Split array objects into nested arrays of 3 objects each
+// for (i = 0; i < valuesArray1.length; i++) {
+//   valuesString[i].split(`\n`);
+// };
+// console.log(valuesArray1)
+
 
 // Draw a map
 let valuesMap = {
